6967d2aedb991835216f99b12322f26160b14c25
UnholyCreations <UnholyCreations@protonmail.com>
2017-08-17 18:15:24 +0300

    ray transofrmation

--- framework/ray.cpp
+++ framework/ray.cpp
@@ -1,10 +1,22 @@
 #include "ray.hpp"
     Ray::Ray():
     m_origin{glm::vec3{0,0,0}},
-    m_direction{glm::vec3{-1,-1,-1}}
+    m_direction{glm::vec3{0,0,0}},
+    m_inverse{glm::vec3{-0,-0,-0}}
     {}
 
-    Ray::Ray(glm::vec3 origin, glm::vec3 direction):
+    Ray::Ray(glm::vec3 origin, glm::vec3 direction,glm::vec3 inverse):
     m_origin{origin},
-    m_direction{direction}
+    m_direction{direction},
+    m_inverse{inverse}
     {}
+	Ray Ray::transformRay(glm::mat4 const& mat)
+	{
+	  glm::vec4 temp_org {m_origin, 1.0f};
+	  glm::vec4 temp_dir {m_direction, 0.0f};
+	  glm::vec3 temp2_org {mat * temp_org };
+	  glm::vec3 temp2_dir {mat * temp_dir};
+	  m_origin = temp2_org;
+	  m_direction = glm::normalize(temp2_dir);
+	  return *this;
+	}
--- framework/ray.hpp
+++ framework/ray.hpp
@@ -1,16 +1,21 @@
 #ifndef RAY_HPP
 #define RAY_HPP
 
-#include <glm/vec3.hpp>
+#include <glm/glm.hpp>
+//include <glm/vec3.hpp>
 
 struct Ray
 {
     glm::vec3 m_origin;
     glm::vec3 m_direction;
-
+    glm::vec3 m_inverse;
     Ray();
-    Ray(glm::vec3 origin, glm::vec3 direction);
+    Ray(glm::vec3 origin, glm::vec3 direction,glm::vec3 inverse);
 
+    glm::vec3 getorigin() const {
+	return m_origin;
+	}
+    Ray transformRay(glm::mat4 const& mat);
 
 };
 
