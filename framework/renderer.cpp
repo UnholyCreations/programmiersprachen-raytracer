ed4bedb5fef80bf223b2d1a3a42a306bcaf44f55
UnholyCreations <UnholyCreations@protonmail.com>
2017-08-31 17:30:37 +0300

    sphere intersection vom scratch gemacht,das Problem mit Schatten kommt nicht von sphere intersect

--- framework/renderer.cpp
+++ framework/renderer.cpp
@@ -100,19 +100,18 @@ Color Is={0,0,0};
 glm::vec3 internorm =glm::normalize(hit.m_norm); //N
 Color Ids={0,0,0};
 Color UnShadows{1,1,1};
-float ShadowBias = 0.01f;
+float ShadowBias = 1.01f;
     for (int i=0;i<scene_.LightVector.size();i++)
         {
           glm::vec3 lightnorm =glm::normalize(hit.m_intersect-scene_.LightVector[i].m_pos);
           
-          Ray shadowray {hit.m_intersect+hit.m_intersect*ShadowBias,scene_.LightVector[i].m_pos};
+          Ray shadowray {hit.m_intersect*ShadowBias,scene_.LightVector[i].m_pos};
           
           for (int i=0;i<scene_.ShapeVector.size();i++)
           {   
           Hit shadowhit=scene_.ShapeVector[i]->intersect(shadowray);
-          if (shadowhit.m_hit==true) {
-              std::cout<<"hit!\n"; 
-              UnShadows={0,0,0};
+          if (shadowhit.m_hit==true) { 
+              //UnShadows={0,0,0};
               break;
             
            }
--- framework/sphere.cpp
+++ framework/sphere.cpp
@@ -56,21 +56,43 @@ return os<<"\n"<<"Sphere: \n"<<"center:"<<m_center.x<<" "<<m_center.y<<" "<<m_ce
 <<"radius:"<<m_radius<<"\n";
 }
 
-//DID WE MISS SMTH?CONSTRUCTORS KILL BRAINCELLS...Struct/class creations functions more...(the easier one)
-//bool hit, float distance, glm::vec3 const& intersect(missing),glm::vec3 const& norm (missing), Shape* shape_ptr
 
 Hit Sphere::intersect(Ray const& ray)
 {
+	
  	Hit sphere_hit;   
+ 	/*
     sphere_hit.m_hit = glm::intersectRaySphere(
     ray.m_origin, glm::normalize(ray.m_direction),
      m_center,m_radius, 
      sphere_hit.m_intersect,sphere_hit.m_norm);
 
+    if (sphere_hit.m_hit==true)
+    {
+
     sphere_hit.m_distance = glm::distance(ray.m_origin, sphere_hit.m_intersect);
 	sphere_hit.m_shape_ptr = this;
-
 	sphere_hit.m_norm=glm::normalize(m_center-sphere_hit.m_intersect);
+	}
+	*/
+
+	
+	float a=glm::dot(ray.m_direction,ray.m_direction);
+	float b=2.0f*glm::dot(ray.m_direction,ray.m_origin-m_center);
+	float c=glm::dot(ray.m_origin-m_center,ray.m_origin-m_center)-m_radius*m_radius;
+	
+	float d = b*b - 4.0f*a*c;
+	if (d>0)
+	{
+		float t1=(-b - sqrt(d))/(2*a);
+		std::cout<<"intersect\n";
+		sphere_hit.m_hit=true;
+		sphere_hit.m_shape_ptr = this;
+		sphere_hit.m_intersect=ray.m_origin+t1*ray.m_direction;
+		sphere_hit.m_distance = glm::distance(ray.m_origin, sphere_hit.m_intersect);
+		sphere_hit.m_norm=glm::normalize(m_center-sphere_hit.m_intersect);
+	}
+	
     return sphere_hit;
     
 }
